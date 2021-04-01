96d3bb2a117e7aee57efaea46d1c55fde7d22c50
Aakrit Subedi <aakritsubedi@pop-os.localdomain>
2021-04-01 20:16:41 +0545

    Array contains tests

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 #### Learning the smart, efficient way to test any JavaScript application.
 ![Type Of Test](./docs/assets/typeOfTest.png)
+
+--- 
+
+### Reference 
+[https://jestjs.io/docs/getting-started](https://jestjs.io/docs/getting-started)
--- tests/passFail.test.js
+++ tests/passFail.test.js
@@ -1,14 +1,18 @@
 const getResult = require('../functions/passFail');
 
+const results = ['Distinction', 'First Division', 'Fail']
+
 test('For Distinction percentage must be greater than or equal to 80', () => {
   let percentage = 86;
   expect(getResult(percentage)).toBe('Distinction'); 
+  expect(results).toContain(getResult(percentage));
   expect(percentage).toBeGreaterThanOrEqual(80);
 });
 
 test('For First Division percentage must be greater less than 80 or greater than 60', () => {
   let percentage = 65;
   expect(getResult(percentage)).toBe('First Division'); 
+  expect(results).toContain(getResult(percentage));
   expect(percentage).toBeGreaterThanOrEqual(60);
   expect(percentage).toBeLessThan(80);
 });
@@ -16,5 +20,6 @@ test('For First Division percentage must be greater less than 80 or greater than
 test('Below 60 is considered to be failed', () => {
   let percentage = 45;
   expect(getResult(percentage)).toBe('Fail'); 
+  expect(results).toContain(getResult(percentage));
   expect(percentage).toBeLessThan(60);
 });
